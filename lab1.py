d4795931819172de1abef56d499a227409830d73
Otito <onwudiweotito@gmail.com>
2026-02-13 00:06:40 +0000

    ROI cropping

--- lab1.py
+++ lab1.py
@@ -1,16 +1,22 @@
 import cv2
+import numpy as np
 
 img = cv2.imread("test.jpg")
 
-if img is None:
-    print("Image did not load")
-else:
-    img[100:150, 100:150] = [0, 200, 0]
+b, g, r = cv2.split(img)
 
-    cv2.imshow("Modified Image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-  
-    print("Color shape:", img.shape)
-    print("Gray shape:", gray.shape)
- 
+# Create images where only one channel is kept
+blue_img = cv2.merge([b, np.zeros_like(b), np.zeros_like(b)])
+green_img = cv2.merge([np.zeros_like(g), g, np.zeros_like(g)])
+red_img = cv2.merge([np.zeros_like(r), np.zeros_like(r), r])
+
+cv2.imshow("Original", img)
+cv2.imshow("Blue Only", blue_img)
+cv2.imshow("Green Only", green_img)
+cv2.imshow("Red Only", red_img)
+
+roi = img[200:600, 300:700]
+cv2.imshow("ROI", roi)  
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
